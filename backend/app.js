228e9bf67bd451a78d9b5261ad365943d8b2ef37
AnnaMVSPB <makarobaanya@yandex.ru>
2022-12-02 12:16:11 +0300

    deploy

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,14 @@ const session = require('express-session');
 const apiRoute = require('./Routes/apiRoute');
 const authRoute = require('./Routes/authRoute');
 const sessionConfig = require('./config/session');
+require('dotenv').config();
 
 const app = express();
 
 const PORT = 4000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin: ['http://localhost:3000'],credentials: true}));
+app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(session(sessionConfig));
